9bec541f4edf87126e14f0a1d72330baa6361023
holynekk <e238109@metu.edu.tr>
2022-05-05 02:26:04 +0300

    home view & bootstrap

--- tweets/urls.py
+++ tweets/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path, re_path
-from .views import home_view, tweet_detail_view
+from .views import home_view, tweet_detail_view, tweet_list_view
 
 urlpatterns = [
     path('', home_view),
+    path('tweets/', tweet_list_view),
     path('tweets/<int:tweet_id>', tweet_detail_view),
 ]
--- tweets/views.py
+++ tweets/views.py
@@ -6,7 +6,15 @@ from .models import Tweet
 # Create your views here.
 
 def home_view(request, *args, **kwargs):
-    return HttpResponse("<h1>Hello Moruk</h1>")
+    return render(request, 'pages/home.html', context={}, status=200)
+
+def tweet_list_view(request, *args, **kwargs):
+    query_set = Tweet.objects.all()
+    tweet_list = [{'id': x.id, "content": x.content} for x in query_set]
+    data = {
+        'response': tweet_list,
+    }
+    return JsonResponse(data)
 
 def tweet_detail_view(request, tweet_id, *args, **kwargs):
     data = {
