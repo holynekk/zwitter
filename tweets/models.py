229251ba4710923ccc8b2e8e0e629dd67dd48e52
holynekk <e238109@metu.edu.tr>
2022-06-15 14:56:30 +0300

    admin registration user model

--- tweets/models.py
+++ tweets/models.py
@@ -1,10 +1,16 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+User = settings.AUTH_USER_MODEL
 
 class Tweet(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.TextField(max_length=280, blank=True, null=True)
     image = models.FileField(upload_to='images/', blank=True, null=True)
+
+    def __str__(self):
+        return self.content
 
     class Meta:
         ordering = ['-id']
