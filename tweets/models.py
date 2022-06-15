13b3aceaa6b9b5103308540e8e457d94e1ab0849
holynekk <e238109@metu.edu.tr>
2022-06-15 16:36:17 +0300

    zweet like model

--- tweets/models.py
+++ tweets/models.py
@@ -4,10 +4,17 @@ from django.conf import settings
 # Create your models here.
 User = settings.AUTH_USER_MODEL
 
+class TweetLike(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    tweet = models.ForeignKey('Tweet', on_delete=models.CASCADE)
+    time_stamp = models.DateTimeField(auto_now_add=True)
+
 class Tweet(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    likes = models.ManyToManyField(User, related_name='tweet_user', blank=True, through=TweetLike)
     content = models.TextField(max_length=280, blank=True, null=True)
     image = models.FileField(upload_to='images/', blank=True, null=True)
+    time_stamp = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.content
